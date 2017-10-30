f0e1ce5bcc86a2f4c4474f0d3c08ecf63eee5bae
Krzysztof Lema <lema10@o2.pl>
2017-10-30 15:04:25 +0100

    URL Connection added

---
+++ src/main/java/plKrzysztofLema/Models/Config.java
@@ -0,0 +1,7 @@
+package plKrzysztofLema.Models;
+
+public class Config {
+    public static final String APP_ID = "8dc379acad6ee71b6d730e293aa8efc7";
+    public static final String APP_BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+}
+
